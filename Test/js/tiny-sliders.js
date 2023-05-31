a76d2c8de405fcd90f3b3aea1ae94208b7a34ac0
vythetran2002 <binclun2002@gmail.com>
2023-05-31 20:46:15 +0700

    add ngay 31 thang 5 2023

--- Test/js/tiny-sliders.js
+++ Test/js/tiny-sliders.js
@@ -1,11 +1,15 @@
 var slider = tns({
     container: '.my-slider',
-    items:3,
+    items: 3,
     controls: true,
-    nav: false,
-    gutter: 20,
+    gutter: 40,
+    //edgePadding: 20,
     loop: true,
-    autoplay : true,
+    nav: false,
+    controlsPosition: 'bottom',
+    slideBy: 1,
+    mouseDrag: true,
+    autoplay: true,
     autoplayButtonOutput: false,
     controlsContainer: '#custom-control'
-})
+});
